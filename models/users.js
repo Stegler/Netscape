c2c4f513ad5020d8eaeadd8bf38abcd5fcacc922
Elvy Kiung <kiungh22@gmail.com>
2019-07-10 19:41:41 -0700

    add game file and set up server directory

---
+++ models/users.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define('Users', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    treasurePoint: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    bestTime: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  });
+  return Users;
+};
