8241a8e0884db19475209dc63f420ed149aedf86
Elvy Kiung <kiungh22@gmail.com>
2019-07-11 22:41:36 -0700

    fix the sequalize sync

--- server.js
+++ server.js
@@ -34,25 +34,21 @@ require('./routes/htmlRoutes')(app);
 //   res.send('OK');
 // });
 
-app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
-});
-
 //===================
-
+//Listen
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
-// clearing the `testdb`
-// if (process.env.NODE_ENV === 'test') {
-//   syncOptions.force = true;
-// }
 
-// // Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync().then(function() {
-//   app.listen(PORT, function() {
-//     console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
-//   });
-// });
+if (process.env.NODE_ENV === 'test') {
+  syncOptions.force = true;
+}
 
-// module.exports = app;
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+  });
+});
+
+module.exports = app;
