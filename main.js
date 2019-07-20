a0f1f0f4320add0b7a1070c2b72518f6811297a0
Elvy Kiung <kiungh22@gmail.com>
2019-07-19 23:53:23 -0700

    set full screen

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, globalShortcut } = require("electron");
 const path = require("path");
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -19,7 +19,7 @@ function createWindow() {
     minHeight: 800,
     resizable: true,
     center: true,
-    fullscreenable: true,
+    fullscreen: true,
     autoHideMenuBar: true
   });
 
@@ -58,3 +58,9 @@ app.on("activate", function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+app.on("ready", () => {
+  globalShortcut.register("CommandOrControl+X", () => {
+    console.log("CommandOrControl+X is pressed");
+    app.quit();
+  });
+});
